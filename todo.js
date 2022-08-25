865dfa994ebc6606ad80a86ff5231cb82a315bd3
shamichealous <ishamichealous@gmail.com>
2022-08-24 20:26:08 -0500

    pushing todo list

---
+++ todo.js
@@ -0,0 +1,36 @@
+    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+    let arrayOfTodos = [
+        {
+        "userId": 14,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 20,
+        "id": 2,
+        "title": "delectus aut autem",
+        "completed": false
+    }]
+    
+    const fetchTodos = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then( (response) => response.json())
+        .then( (json) => arrayOfTodos = json)
+    }
+    
+    const logTodos = () => {
+        console.log(arrayOfTodos)
+    }
+    
+    const populateTodos = () => {
+        let parent = document.querySelector("ol")
+        arrayOfTodos.forEach(todo =>{
+            console.log(todo)
+            let child = document.createElement("li")
+            child.innerText = todo.title
+            parent.appendChild(child)
+        })
+    
+    }
